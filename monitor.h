f364ce1faef9047d0a5fbd52ddaabefddbe74cba
PRANEETHDPV <67943209+PRANEETHDPV@users.noreply.github.com>
2025-09-17 18:34:42 +0530

    update

--- monitor.h
+++ monitor.h
@@ -1,8 +1,9 @@
 #pragma once
 
 int vitalsOk(float temperature, float pulseRate, float spo2);
-int vit_Temp(float temperature);
-int vit_PulseRate(float pulseRate);
-int vit_Spo2(float spo2);
+int vital_Temp(float temperature);
+int vital_PulseRate(float pulseRate);
+int vital_Spo2(float spo2);
 void time();
+
 
